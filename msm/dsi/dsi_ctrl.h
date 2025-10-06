39c938a9658af01623d2501e624973bb67206c77
grepfox <grepfox@tutamail.com>
2025-10-06 10:27:32 +0000

    import oplus changes

--- msm/dsi/dsi_ctrl.h
+++ msm/dsi/dsi_ctrl.h
@@ -952,4 +952,6 @@ void dsi_ctrl_toggle_error_interrupt_status(struct dsi_ctrl *dsi_ctrl, bool enab
  * @dsi_ctrl:                 DSI controller handle.
  */
 void dsi_ctrl_transfer_cleanup(struct dsi_ctrl *dsi_ctrl);
+
+int dsi_ctrl_override_dma_cmd_trig(struct dsi_ctrl *dsi_ctrl, enum dsi_trigger_type type);
 #endif /* _DSI_CTRL_H_ */
